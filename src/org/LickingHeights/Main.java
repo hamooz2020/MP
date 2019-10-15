cc598a1bfd2261a97b2e3a3ee3de07e196be59e0
Hamza <hamooz7670@gmail.com>
2019-10-15 10:05:54 -0400

    First Commit

---
+++ src/org/LickingHeights/Main.java
@@ -0,0 +1,23 @@
+package org.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner keyboard;
+        //declare
+        String hello, world;
+
+        //initialize
+
+    public static void printGreetings() {
+        }
+
+
+        
+
+
+    }
+}
