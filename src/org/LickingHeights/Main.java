33623cc8068b16cf04446912b79a621151566ea3
hamooz2020 <55145326+hamooz2020@users.noreply.github.com>
2019-10-18 21:37:53 -0400

    Added method

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -5,19 +5,34 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        Scanner keyboard;
-        //declare
-        String hello, world;
-
-        //initialize
-
-    public static void printGreetings() {
-        }
-
-
+    Scanner keyboard;
         
-
-
-    }
+        System.out.println("Hello World");
+        keyboard = new Scanner (System.in);
+        
+        System.out.println("What's your name?");
+        String greeting;
+        String name;
+        name = " ";
+        name = keyboard.nextLine();
+        System.out.println( "Hello " + name);
+        
+        
+     
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 }
