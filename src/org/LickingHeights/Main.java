4bf07ad951788a936ec0c4773f0b1318217a581f
hamooz2020 <55145326+hamooz2020@users.noreply.github.com>
2019-10-18 21:52:41 -0400

    added more methods

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -11,12 +11,29 @@ public class Main {
         keyboard = new Scanner (System.in);
         
         System.out.println("What's your name?");
-        String greeting;
-        String name;
+        String greeting,name;
+   
         name = " ";
         name = keyboard.nextLine();
         System.out.println( "Hello " + name);
         
+        int a,b;
+        a=10
+        b=15
+            int answer = multiply(a,b)
+            System.out.println(answer)
+            //Answer 2
+            int c,d;
+            e = 1;
+            f = 5;
+            double answer2 = divide(c,d)
+                System.out.println(answer2);
+        //answer3
+        int e,f;
+        e = 1;
+        f = 2;
+        int answer3 = theMax(e,f);
+        System.out.println(answer3)
         
      
         
